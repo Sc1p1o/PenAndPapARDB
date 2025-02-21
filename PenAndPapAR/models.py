4799a0f6fca9e15a945f3bfb0bd35aa883d59617
Martin <martinberuf@gmail.com>
2025-02-21 02:15:00 +0100

    added Items and feats

--- PenAndPapAR/models.py
+++ PenAndPapAR/models.py
@@ -1,26 +1,7 @@
 from django.db import models
 
-class Attributes(models.Model):
-    attribute_name = models.CharField(max_length=20, Primary_key=True)
-    attribute_value = models.PositiveSmallIntegerField(default=10)
-    attribute_adjustment = models.PositiveSmallIntegerField(default=0)
-
-class SavingThrowProficiencies(models.Model):
-    saving_throw_name = models.CharField(max_length=20, Primary_key=True)
-    saving_throw_adjustment = models.PositiveSmallIntegerField(default=0)
-    saving_throw_is_proficient = models.BooleanField(default=False)
-
-class Skills(models.Model):
-    skill_name = models.CharField(max_length=20, Primary_key=True)
-    skill_adjustment = models.PositiveSmallIntegerField(default=0)
-    skill_is_proficient = models.BooleanField(default=False)
-    skill_is_expertise = models.BooleanField(default=False)
-
 class CharacterStats(models.Model):
-    character_id = models.GeregeratedIDField(primary_key=True)
-    character_armor_class = models.PositiveSmallIntegerField(default=10)
-    character_hit_points = models.PositiveSmallIntegerField(default=10)
-    character_movement_speed = models.PositiveSmallIntegerField(default=30)
+    character_id = models.AutoField(primary_key=True)
     character_is_inspired = models.BooleanField(default=False)
     character_name = models.CharField(max_length=50)
     character_class = models.CharField(max_length=50)
@@ -28,12 +9,45 @@ class CharacterStats(models.Model):
     character_background = models.CharField(max_length=50)
     character_subclass = models.CharField(max_length=50)
     character_level = models.PositiveSmallIntegerField(default=3)
+    character_alignment = models.CharField(max_length=50)
+    character_conditions = models.TextField()
 
-    character_conditions = models.CharField(max_length=50)
+class Attributes(models.Model):
+    attribute_name = models.CharField(max_length=20, primary_key=True)
+    attribute_value = models.PositiveSmallIntegerField(default=10)
+    attribute_adjustment = models.PositiveSmallIntegerField(default=0)
+    attribute_charakter = models.ForeignKey(CharacterStats, on_delete=models.CASCADE)
 
+class AC(models.Model):
+    AC_base = models.CharField(max_length=20, primary_key=True)
+    AC_modified = models.PositiveSmallIntegerField(default=10)
+    attribute_charakter = models.ForeignKey(CharacterStats, on_delete=models.CASCADE)
+
+class SavingThrowProficiencies(models.Model):
+    saving_throw_name = models.CharField(max_length=20, primary_key=True)
+    saving_throw_adjustment = models.PositiveSmallIntegerField(default=0)
+    saving_throw_is_proficient = models.BooleanField(default=False)
+    attribute_charakter = models.ForeignKey(CharacterStats, on_delete=models.CASCADE)
+
+class Skills(models.Model):
+    skill_name = models.CharField(max_length=20, primary_key=True)
+    skill_adjustment = models.PositiveSmallIntegerField(default=0)
+    skill_is_proficient = models.BooleanField(default=False)
+    skill_is_expertise = models.BooleanField(default=False)
+    attribute_charakter = models.ForeignKey(CharacterStats, on_delete=models.CASCADE)
+
+class HitPoints(models.Model):
+    hit_points_current = models.PositiveSmallIntegerField(default=10)
+    hit_points_max = models.PositiveSmallIntegerField(default=10)
+    hit_points_temp = models.PositiveSmallIntegerField(default=0)
+    non_lethal_damage = models.PositiveSmallIntegerField(default=0)
+    attribute_charakter = models.ForeignKey(CharacterStats, on_delete=models.CASCADE)
+
+
+"""
 class Items(models.Model):
     # Item Characteristics
-    item_name = models.CharField(max_length=50, Primary_key=True)
+    item_name = models.CharField(max_length=50, primary_key=True)
     item_weight = models.PositiveSmallIntegerField(default=10)
     item_value = models.PositiveSmallIntegerField(default=10)
     item_is_equipped = models.BooleanField(default=False)
@@ -47,7 +61,7 @@ class Items(models.Model):
 
 class Feats(models.Model):
     # Item Characteristics
-    feat_name = models.CharField(max_length=50, Primary_key=True)
+    feat_name = models.CharField(max_length=50, primary_key=True)
     feat_description = models.TextField()
 
     # Modifiers
@@ -56,3 +70,4 @@ class Feats(models.Model):
     feat_modified_saving_throws = models.ManyToManyField('SavingThrowProficiencies',
                                                          related_name='feat_modified_saving_throws')
     feat_modified_stats = models.ManyToManyField('CharacterStats', related_name='feat_modified_stats')
+"""
